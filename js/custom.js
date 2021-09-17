7c4d19709c6996f8dad3612ca7ca473ae5087285
Leandro Mondin <leandro.mondin@mercadolibre.com>
2021-09-16 21:19:10 -0300

    Change in language's section

--- js/custom.js
+++ js/custom.js
@@ -21,6 +21,21 @@ function ingles(){
     $("#navViaje").first().append("My travel");
     $("#navContacto").first().empty();
     $("#navContacto").first().append("Contact");
+
+    $("#followMe").first().empty();
+    $("#followMe").first().append("Follow me!");
+    $("#picture01").first().empty();
+    $("#picture01").first().append("Be happy. Always!");
+    $("#picture02").first().empty();
+    $("#picture02").first().append("The things won't be like before. They will be better!");
+    $("#picture03").first().empty();
+    $("#picture03").first().append("I can arrive to the infinite.");
+    $("#h1Trabajo").first().empty();
+    $("#h1Trabajo").first().append("Work");
+
+    $("#puestoMeli").first().empty();
+    $("#puestoMeli").first().append("Sr IAM Cyber Security Engineer in Mercado Libre");
+
     $("#ingles").animate({fontSize:'130%',opacity:'1'}, 500,function(){});
     $("#espaniol").animate({fontSize:'100%',opacity:'0.3'}, 500,function(){});
 }
@@ -40,4 +55,19 @@ function espaniol(){
     $("#navViaje").first().append("Mi viaje");
     $("#navContacto").first().empty();
     $("#navContacto").first().append("Contacto");
+    $("#followMe").first().empty();
+    $("#followMe").first().append("Sigueme!");
+
+    $("#picture01").first().empty();
+    $("#picture01").first().append("Se feliz. Siempre.");
+    $("#picture02").first().empty();
+    $("#picture02").first().append("Las cosas no serán como antes, serán mejor");
+    $("#picture03").first().empty();
+    $("#picture03").first().append("Puedo llegar al infinito.");
+    $("#h1Trabajo").first().empty();
+    $("#h1Trabajo").first().append("Trabajo");
+    
+    $("#puestoMeli").first().empty();
+    $("#puestoMeli").first().append("Ingeniero Sr - Seguridad Informatica IAM en Mercado Libre");
+    
 }
